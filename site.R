8078fb19ee67b2526df11a8d76387e55a14cd5c4
Margarita <47417976+MargoSolo@users.noreply.github.com>
2023-04-01 05:18:05 +0300

    Create site.R

---
+++ site.R
@@ -0,0 +1,5 @@
+install.packages("distill")
+
+#distill::create_blog(dir = "amazing blog", title = "A cool name of my blog")
+# or
+distill::create_website(dir = "amazing website", title = "DoctorShke")
