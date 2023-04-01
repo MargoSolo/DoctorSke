27c75c15656ada36bf606a72818940bdfd4ca44c
Margarita <47417976+MargoSolo@users.noreply.github.com>
2023-04-01 05:27:13 +0300

    s

--- site.R
+++ site.R
@@ -1,5 +1,7 @@
-install.packages("distill")
+#install.packages("distill")
 
 #distill::create_blog(dir = "amazing blog", title = "A cool name of my blog")
 # or
+
+setwd("C:/Users/Margarita/Documents/GitHub/DoctorSke")
 distill::create_website(dir = "amazing website", title = "DoctorShke")
