2708a9cfdded85fedcfc2d8c62ddbd1b4bd8805c
Margarita <47417976+MargoSolo@users.noreply.github.com>
2023-04-01 05:53:29 +0300

    1

--- README.md
+++
@@ -1,2 +0,0 @@
-# DoctorSke
-personal website
--- site.R
+++
@@ -1,7 +0,0 @@
-#install.packages("distill")
-
-#distill::create_blog(dir = "amazing blog", title = "A cool name of my blog")
-# or
-
-setwd("C:/Users/Margarita/Documents/GitHub/DoctorSke")
-distill::create_website(dir = "amazing website", title = "DoctorShke")
